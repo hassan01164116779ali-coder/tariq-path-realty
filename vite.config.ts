bd0472ba282d92c3b666e4ae53b48234b815743d
hassan <hassan01164116779ali@gmail.com>
2025-11-20 04:00:58 +0200

    my first site

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,31 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+
+
+// export default defineConfig({
+//   base: '/tariq-path-realty/',
+//   plugins: [react()],
+// })
+
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-}));
+  base: '/tariq-path-realty/',
+})
